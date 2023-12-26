caab6cc9fb8416bcbce8ebb13b5758b21b0d7df6
crazyvillage <mag@crazyvillage.net>
2023-12-26 14:37:38 -0800

    add xplat project

---
+++ xplat/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace xplat;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ xplat/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace xplat;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
